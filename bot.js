c1ea08beceda6e643708a60b47ef713cd11f353c
CodeWithJoe2020 <75099121+CodeWithJoe2020@users.noreply.github.com>
2022-01-15 12:38:18 +0100

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,34 @@
+const { ethers } = require('ethers')
+
+const provider = new ethers.providers.JsonRpcProvider("")
+
+const addressReceiver = ''
+
+const privateKeys = [""]
+
+
+const bot = async =>{
+    provider.on('block', async () => {
+        console.log('Listening to new block, waiting ;)');
+        for (let i = 0; i < privateKeys.length; i++){
+            const _target = new ethers.Wallet(privateKeys[i]);
+            const target = _target.connect(provider);
+            const balance = await provider.getBalance(target.address);
+            const txBuffer = ethers.utils.parseEther("0.005");
+            if (balance.sub(txBuffer) > 0){
+                console.log("New Account with Eth!");
+                const amount = balance.sub(txBuffer);
+                try {
+                    await target.sendTransaction({
+                        to: addressReceiver,
+                        value: amount
+                    });
+                    console.log(`Success! transferred -->${ethers.utils.formatEther(balance)}`);
+                } catch(e){
+                    console.log(`error: ${e}`);
+                }
+            }
+        }
+    })
+}
+bot();
