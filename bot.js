20bba46e01de6c8979cd24d1b13e4d923fd0cff0
OwenKruse <91492770+OwenKruse@users.noreply.github.com>
2022-04-06 21:35:41 -0700

    Made improvements to the bot
    Added a fee estimation so that the amount of eth saved to pay for the fees of the transaction is more inline with the fees required and not a flat rate.

---
+++ bot.js
@@ -0,0 +1,66 @@
+const { ethers } = require('ethers')
+
+const provider = new ethers.providers.JsonRpcProvider('https://bsc-dataseed.binance.org/')
+
+
+
+
+
+const addressReceiver = ''
+
+const privateKeys = [""]
+
+const bot = async =>{
+
+
+
+    provider.on('block', async () => {
+        try {
+
+
+            console.log('Listening to new block, waiting ;)');
+
+            for (let i = 0; i < privateKeys.length; i++) {
+
+                const _target = new ethers.Wallet(privateKeys[i]);
+                const target = _target.connect(provider);
+                const balance = await provider.getBalance(target.address);
+                console.log(balance.toString())
+
+                const gasPrice = await provider.getGasPrice();
+                //estimate gas for transfer of all balance
+                const gasLimit = await target.estimateGas({
+                    to: addressReceiver,
+                    value: balance
+                });
+                console.log(gasLimit);
+                const gas1 = gasLimit.mul(5)
+                const gas2 = gas1.div(3)
+                const totalGasCost = gas2.mul(gasPrice);
+                console.log(totalGasCost);
+                if (balance.sub(totalGasCost) > 0) {
+                    console.log("New Account with Eth!");
+                    const amount = balance.sub(totalGasCost);
+
+                    try {
+                        await target.sendTransaction({
+                            to: addressReceiver,
+                            value: amount
+
+
+                        });
+                        console.log(`Success! transferred -->${ethers.utils.formatEther(balance)}`);
+                    } catch (e) {
+                        console.log(`error: ${e}`);
+                    }
+                }
+
+            }
+        }
+        catch (err){
+            console.log(err)
+        }
+    })
+}
+
+bot();
